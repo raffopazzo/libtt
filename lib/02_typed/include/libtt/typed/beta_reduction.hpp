c5ec2223d3cf19d6c59c54683b2ba17238243cbe
Raffaele Rossi <raffaele.rossi@mavensecurities.com>
2023-03-01 13:49:35 +0000

    cleanup

--- lib/02_typed/include/libtt/typed/beta_reduction.hpp
+++ lib/02_typed/include/libtt/typed/beta_reduction.hpp
@@ -2,19 +2,12 @@
 
 #include "libtt/typed/legal_term.hpp"
 
-#include <optional>
-#include <vector>
-
 namespace libtt::typed {
 
 bool is_redex(legal_term const&);
 std::size_t num_redexes(legal_term const&);
-std::vector<legal_term> one_step_beta_reductions(legal_term const&);
-legal_term beta_normalize(legal_term const&);
-
 inline bool is_beta_normal(legal_term const& x) { return num_redexes(x) == 0ul; }
 
-bool beta_reduces_to(legal_term const&, legal_term const&);
-bool is_beta_equivalent(legal_term const&, legal_term const&);
+legal_term beta_normalize(legal_term const&);
 
 }
